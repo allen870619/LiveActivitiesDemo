8af7e7e8e0149c8e9cbdee0e2017829d29973dda
Allen Lee <allen870619@gmail.com>
2023-08-13 15:51:52 +0800

    Initial Commit

---
+++ ActivityKitDemo/ActivityKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityKitDemoApp.swift
+//  ActivityKitDemo
+//
+//  Created by Allen Lee on 2023/8/13.
+//
+
+import SwiftUI
+
+@main
+struct ActivityKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
